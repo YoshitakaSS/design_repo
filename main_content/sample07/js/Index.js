d9141bcfa5261820fd91eaef8e9ad668647582f3
ys <yoshi.23unog34@gmail.com>
2019-09-07 12:22:54 +0900

    @update デザイン専用リポジトリを最新化

--- main_content/sample07/js/Index.js
+++ main_content/sample07/js/Index.js
@@ -2,6 +2,8 @@ $(function(){
     var $navBar = $('.nav-bar');
     var $menu = $('.header-menu');
 
+
+
     $navBar.on('click', function(){
         if ($(this).hasClass('fa-bars')) {
             $(this).removeClass('fa-bars');
@@ -15,4 +17,42 @@ $(function(){
             $menu.slideUp();
         }
     });
+
+    /**
+     * サジェスト一覧を取得する
+     */
+    function getSuggestKeywordInfo()
+    {
+        $.ajax({
+            url: '/top/book/suggest',
+            type: 'POST',
+            dataType: 'json',
+            timeout: 5000,
+        })
+        .done( (data) => {
+            // 何も取得できない場合は何もしない
+            if (!data || data.length === 0) {
+                alert('何も取得できませんでした。キーワードを変えてみてください');
+                return;
+            }
+        })
+        .fail( (data) => {
+            alert('通信に失敗しました');
+        });
+    }
+
+    /**
+     * 文字を入力したら、推奨検索ワードを入力
+     */
+    function addSuggestList()
+    {
+        var searchForm = document.getElementById('js-top-search');
+        var ul = document.createElement('ul');
+        var li = document.createElement('li');
+        var a = document.createElement('a');
+
+        ul.classList.add('suggest-list');
+        a.setAttribute('href', '#');
+    }
+
 });
